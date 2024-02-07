01464a7f8aaa8a3c6eeab7f20e4c62986f219790
suresh298877 <sureshpeddimsetti298@gmail.com>
2024-02-07 22:45:22 +0530

    video stopped at 10:38:32

--- projects/7-social-media/src/components/Sidebar.jsx
+++ projects/7-social-media/src/components/Sidebar.jsx
@@ -1,4 +1,6 @@
-const Sidebar = () => {
+const Sidebar = ({selectedTab,setSelectedTab}) => {
+
+    console.log(selectedTab);
     return <>
     <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar" style={{width: '280px'}}>
         <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
@@ -7,14 +9,14 @@ const Sidebar = () => {
         </a>
         <hr />
         <ul className="nav nav-pills flex-column mb-auto">
-            <li className="nav-item">
-                <a href="#" className="nav-link active" aria-current="page">
+            <li className="nav-item" onClick={()=>setSelectedTab('Home')}>
+                <a href="#" className={`nav-link text-white ${selectedTab === "Home" && "active"}`} aria-current="page">
                     <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="#home"></use></svg>
                     Home
                 </a>
             </li>
-            <li>
-                <a href="#" className="nav-link text-white">
+            <li onClick={()=>setSelectedTab('Create Post')}>
+                <a href="#" className={`nav-link text-white ${selectedTab === "Create Post" && "active"}`}>
                     <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="#speedometer2"></use></svg>
                     Create Post
                 </a>
---
+++ projects/7-social-media/src/store/post-list-store.jsx
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+const PostList=createContext([]);
+
+const PostListProvider=({children})=>{
+    return <PostList.Provider value={[]}>
+        {children}
+    </PostList.Provider>
+}
+
+export default PostListProvider;
