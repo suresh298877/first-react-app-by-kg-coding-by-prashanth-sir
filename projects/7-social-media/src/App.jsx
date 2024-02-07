4795dca842769a7694cae8b3260a850876688b93
unknown <sureshpeddimsetti298@gmail.com>
2024-02-07 20:05:26 +0530

    7/2/24 some project pushed forward and stopped video at 10 hours

--- projects/7-social-media/src/App.jsx
+++ projects/7-social-media/src/App.jsx
@@ -1,11 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Sidebar from './components/Sidebar'
+import CreatePost from './components/CreatePost'
+import PostList from './components/PostList'
 function App() {
-  return <>
-    <Header></Header>
-    <Footer></Footer>
-  </>
+  return <div className="app-container">
+    <Sidebar></Sidebar>
+    <div className="content">
+      <Header></Header>
+      <CreatePost></CreatePost>
+      <PostList></PostList>
+      <Footer></Footer>
+    </div>
+  </div>
 }
 
 export default App
