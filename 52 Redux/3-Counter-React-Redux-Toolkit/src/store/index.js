da863a15c0aa6aa440e637adbd02bd981685b53e
suresh298877 <sureshpeddimsetti298@gmail.com>
2024-02-14 15:17:25 +0530

    video stopped at 16:49:00

--- 52 Redux/3-Counter-React-Redux-Toolkit/src/store/index.js
+++ 52 Redux/3-Counter-React-Redux-Toolkit/src/store/index.js
@@ -1,31 +1,36 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 
 const INITIAL_VALUE = {
     counter : 5,
     privacy : false,
 }
 
-const counterReducer = ( store = INITIAL_VALUE, action ) => {
+// const counterReducer = ( store = INITIAL_VALUE, action ) => {
 
-    if( action.type === "INCREMENT" ){
-        return { ...store, counter : store.counter + 1 }
-    }
-    else if( action.type === "DECREMENT" ){
-        return {...store, counter : store.counter -1 }
-    }
-    else if ( action.type === "Add" ){
-        return {...store, counter : store.counter + Number(action.payload.num) }
-    }
-    else if( action.type === "Subtract" ){
-        return {...store, counter : store.counter - Number(action.payload.num) }
-    }
-    else if( action.type === "toggle" ){
-        return { ...store, privacy : !store.privacy }
-    }
-    return store;
-};
+//     if( action.type === "INCREMENT" ){
+//         return { ...store, counter : store.counter + 1 }
+//     }
+//     else if( action.type === "DECREMENT" ){
+//         return {...store, counter : store.counter -1 }
+//     }
+//     else if ( action.type === "Add" ){
+//         return {...store, counter : store.counter + Number(action.payload.num) }
+//     }
+//     else if( action.type === "Subtract" ){
+//         return {...store, counter : store.counter - Number(action.payload.num) }
+//     }
+//     else if( action.type === "toggle" ){
+//         return { ...store, privacy : !store.privacy }
+//     }
+//     return store;
+// };
 
-const counterStore=createStore( counterReducer );
+
+
+const counterStore=configureStore( { reducer : {
+    
+} } );
 
 
 export default counterStore;
